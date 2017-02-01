7e6615e3d32a06f9a6905b38724c9815690317c6
Oscar Farg <oscar.farg@ccgroup.nl>
2017-02-01 11:42:32 +0100

    Removed mockito dependency

--- src/test/java/nl/stil4m/mollie/DefaultObjectMappingTest.java
+++ src/test/java/nl/stil4m/mollie/DefaultObjectMappingTest.java
@@ -1,13 +1,13 @@
 package nl.stil4m.mollie;
 
+import java.lang.reflect.Field;
+
+import org.junit.Test;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
+
 import nl.stil4m.mollie.mocks.DummyMethod;
-import org.junit.Test;
-import org.mockito.internal.util.reflection.Whitebox;
-
-import java.io.IOException;
-
 import static junit.framework.TestCase.fail;
 import static nl.stil4m.mollie.TestUtil.strictDynamicClientWithApiKey;
 import static org.hamcrest.CoreMatchers.is;
@@ -16,19 +16,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class DefaultObjectMappingTest {
 
     @Test
-    public void testShouldNotFailByDefaultOnAdditionalFields() throws IOException {
+    public void testShouldNotFailByDefaultOnAdditionalFields() throws Exception {
         DynamicClient client = new DynamicClientBuilder().build();
-        RequestExecutor requestExecutor = (RequestExecutor) Whitebox.getInternalState(client, "requestExecutor");
-        ObjectMapper objectMapper = (ObjectMapper) Whitebox.getInternalState(requestExecutor, "objectMapper");
+        RequestExecutor requestExecutor = (RequestExecutor) getInternalField(client, "requestExecutor");
+        ObjectMapper objectMapper = (ObjectMapper) getInternalField(requestExecutor, "objectMapper");
 
         objectMapper.readValue("{\"id\":\"x\", \"name\":\"Foo\"}", DummyMethod.class);
     }
 
     @Test
-    public void testShouldFailForStrictObjectMapperOnAdditionalFields() throws IOException {
+    public void testShouldFailForStrictObjectMapperOnAdditionalFields() throws Exception {
         DynamicClient client = strictDynamicClientWithApiKey();
-        RequestExecutor requestExecutor = (RequestExecutor) Whitebox.getInternalState(client, "requestExecutor");
-        ObjectMapper objectMapper = (ObjectMapper) Whitebox.getInternalState(requestExecutor, "objectMapper");
+        RequestExecutor requestExecutor = (RequestExecutor) getInternalField(client, "requestExecutor");
+        ObjectMapper objectMapper = (ObjectMapper) getInternalField(requestExecutor, "objectMapper");
 
         try {
             objectMapper.readValue("{\"id\":\"x\", \"name\":\"Foo\"}", DummyMethod.class);
@@ -37,5 +37,17 @@ public class DefaultObjectMappingTest {
             assertThat(e.getPropertyName(), is("name"));
         }
     }
+
+    private static <T> Object getInternalField(T target, String field) throws IllegalAccessException {
+        try {
+            Class<?> targetClass = target.getClass();
+            Field cField = targetClass.getDeclaredField(field);
+            cField.setAccessible(true);
+            return cField.get(target);
+        } catch (NoSuchFieldException e) {
+            return null;
+        }
+    }
+
 
 }
